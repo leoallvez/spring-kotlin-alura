02d268eb6ae71b701f14c1b6e24255f1b984379c
Leonardo Alves <leoallvez@hotmail.com>
2023-11-02 00:06:45 -0300

    feat: busca de tópico por id

--- src/main/kotlin/io/github/leoallvez/forum/controller/TopicController.kt
+++ src/main/kotlin/io/github/leoallvez/forum/controller/TopicController.kt
@@ -3,6 +3,8 @@ package io.github.leoallvez.forum.controller
 import io.github.leoallvez.forum.model.Topic
 import io.github.leoallvez.forum.service.TopicService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,4 +16,10 @@ class TopicController(private val service: TopicService) {
     fun list(): List<Topic> {
         return service.list()
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): Topic {
+        return service.findById(id)
+    }
+
 }
--- src/main/kotlin/io/github/leoallvez/forum/service/TopicService.kt
+++ src/main/kotlin/io/github/leoallvez/forum/service/TopicService.kt
@@ -6,25 +6,49 @@ import io.github.leoallvez.forum.model.User
 import org.springframework.stereotype.Service
 
 @Service
-class TopicService {
+class TopicService(private var topics: List<Topic>) {
+
+    init {
+        val course = Course(
+            id = 1,
+            name = "Kotlin",
+            category = "Programming"
+        )
+        val user = User(
+            id = 1,
+            name = "Leo",
+            email = "leoallvez@gmail.com"
+        )
+        val topic1 = Topic(
+            id = 1,
+            title = "Kotlin doubts 1",
+            message = "Kotlin Variables 1",
+            course = course,
+            author = user
+        )
+        val topic2 = Topic(
+            id = 2,
+            title = "Kotlin doubts 2",
+            message = "Kotlin Variables 2",
+            course = course,
+            author = user
+        )
+        val topic3 = Topic(
+            id = 3,
+            title = "Kotlin doubts 3",
+            message = "Kotlin Variables 3",
+            course = course,
+            author = user
+        )
+        topics = listOf(topic1, topic2, topic3)
+    }
 
     fun list(): List<Topic> {
-        val topic = Topic(
-            id = 1,
-            title = "Kotlin doubts",
-            message = "Kotlin Variables",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programming"
-            ),
-            author = User(
-                id = 1,
-                name = "Leo",
-                email = "leoallvez@gmail.com"
-            )
-        )
-        return listOf(topic, topic, topic, topic)
+        return topics
+    }
+
+    fun findById(id: Long): Topic {
+        return topics.stream().filter { t -> t.id == id }.findFirst().get()
     }
 
 }
